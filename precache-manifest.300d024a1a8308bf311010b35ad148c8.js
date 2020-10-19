46d7f6961b17b85297ba333c4d68c2a022c88337
Margokomilfo-dev <margokomilfo@mail.ru>
2020-10-20 01:00:10 +0200

    Updates

---
+++ precache-manifest.300d024a1a8308bf311010b35ad148c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10b9ef64e5f389b9176bb82ae5d20a0",
+    "url": "/tasks_from_Ignat/index.html"
+  },
+  {
+    "revision": "e46f38b10a7e7aad2dc2",
+    "url": "/tasks_from_Ignat/static/css/main.ae0b9869.chunk.css"
+  },
+  {
+    "revision": "4ff1ab5b4389726c92c7",
+    "url": "/tasks_from_Ignat/static/js/2.7262d58d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tasks_from_Ignat/static/js/2.7262d58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46f38b10a7e7aad2dc2",
+    "url": "/tasks_from_Ignat/static/js/main.62e88196.chunk.js"
+  },
+  {
+    "revision": "af1dd24b7e54c6ac668e",
+    "url": "/tasks_from_Ignat/static/js/runtime-main.03990a7a.js"
+  }
+]);
