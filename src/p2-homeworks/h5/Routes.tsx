1f38c4b1eba15f8d67596dc906869e96fad7b0f1
Margokomilfo-dev <margokomilfo@mail.ru>
2020-10-21 13:34:48 +0200

    test commit

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -13,6 +13,19 @@ export const PATH = {
 function Routes() {
     return (
         <div>
+
+           <span> (props.selected === true)
+            ?   <b>Star</b>
+               : 'Star'
+           </span>
+        {/*    return <span className={`${classes.star} +${classes.starActive}`}>*/}
+        {/*    <b>Star</b>*/}
+        {/*    </span>*/}
+        {/*} else {*/}
+        {/*    return <span className={classes.star}>Star</span>*/}
+        {/*}*/}
+
+
             {/*Switch выбирает первый подходящий роут*/}
             <Switch>
 
